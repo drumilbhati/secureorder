5bb5fb697086311c8d0b24b891b2c3ed4456a40b
Drumil Bhati <64512394+drumilbhati@users.noreply.github.com>
2026-04-08 13:45:36 +0530

    Use local header includes in privacy files

--- pkg/privacy/encryption_wrapper.cpp
+++ pkg/privacy/encryption_wrapper.cpp
@@ -1,2 +1,2 @@
-// Include the actual header
-#include "/Users/drumilbhati/Documents/Winter-26/CSE542/secureorder/cpp/include/privacy/encryption.h"
+// Include the actual header.
+#include "privacy/encryption.h"
